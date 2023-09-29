0dc1e0a7953c960c6866a1fa74b5f245d026e98d
bruno <brunoqueiroz612@gmail.com>
2023-09-29 07:54:49 -0300

    Add basic express middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import env from "dotenv";
-
+import cors from "cors";
+import { corsOptions } from "./cors/config";
 env.config();
 
 const app = express();
+
+app.use(express.json());
+app.use(cors(corsOptions));
 
 const port = process.env.PORT || 3333;
 
