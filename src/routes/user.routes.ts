251488d64778d86c108c87a8f7b73303b955bba9
bruno <brunoqueiroz612@gmail.com>
2023-10-04 07:16:33 -0300

    Create sign-in route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { createUserValidationRules } from "../validators/createUserValidator";
 import { createUserController } from "../controllers/createUser";
+import { signInUserController } from "../controllers/signInUser";
+import { signInUserValidationRules } from "../validators/signInUserValidator";
 
 const router = Router();
 
-router.post("/", createUserValidationRules, createUserController);
+router.post("/sign-up", createUserValidationRules, createUserController);
+router.post("/sign-in", signInUserValidationRules, signInUserController);
 
 export default router;
