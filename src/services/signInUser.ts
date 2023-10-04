adc5e1f9a861f10dfecfad0d9d74ebc7469d3aa7
bruno <brunoqueiroz612@gmail.com>
2023-10-04 08:01:13 -0300

    Throw UserNotFound

--- src/services/signInUser.ts
+++ src/services/signInUser.ts
@@ -1,9 +1,14 @@
 import { IncorrectPassword } from "../errors/IncorrectPassword";
+import { UserNotFound } from "../errors/UserNotFound";
 import { userRepository } from "../repositories/userRepository";
 import bcrypt from "bcrypt";
 
 export const signInUser = async (email: string, password: string) => {
-  const userFoundOnDatabase = await userRepository.findUniqueOrThrow(email);
+  const userFoundOnDatabase = await userRepository.findUnique(email);
+
+  if (!userFoundOnDatabase) {
+    throw new UserNotFound("User not found");
+  }
 
   const isUserPasswordCorrect = await bcrypt.compare(
     password,
