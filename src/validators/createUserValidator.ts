5796414b185b0392cba642d9591e44359edcb544
bruno <brunoqueiroz612@gmail.com>
2023-10-05 07:43:17 -0300

    Allow password to be null

--- src/validators/createUserValidator.ts
+++ src/validators/createUserValidator.ts
@@ -3,6 +3,6 @@ import { body } from "express-validator";
 export const createUserValidationRules = [
   body("name").notEmpty().isLength({ min: 2 }),
   body("email").isEmail(),
-  body("password").notEmpty(),
+  body("password").isString(),
   body("picture").optional(),
 ];
--- src/validators/signInUserValidator.ts
+++ src/validators/signInUserValidator.ts
@@ -2,5 +2,5 @@ import { body } from "express-validator";
 
 export const signInUserValidationRules = [
   body("email").isEmail(),
-  body("password").notEmpty(),
+  body("password").isString(),
 ];
