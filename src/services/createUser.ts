39f94244525bbe1c76e09214776acaa7170c4849
bruno <brunoqueiroz612@gmail.com>
2023-10-09 16:53:08 -0300

    Generate random password if not included

--- src/services/createUser.ts
+++ src/services/createUser.ts
@@ -2,7 +2,10 @@ import { User, userRepository } from "../repositories/userRepository";
 import bcrypt from "bcrypt";
 
 export const createUser = async (user: User) => {
-  const passwordHashed = await bcrypt.hash(user.password, 10);
+  const passwordHashed = await bcrypt.hash(
+    user.password || Math.random().toString(),
+    10
+  );
 
   const newUser = await userRepository.createUser({
     ...user,
