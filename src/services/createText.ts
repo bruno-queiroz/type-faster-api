8b51d86abfbfe46deac418c03260668d91e4e6ac
bruno <brunoqueiroz612@gmail.com>
2023-10-12 16:16:01 -0300

    Add default image to text

--- src/services/createText.ts
+++ src/services/createText.ts
@@ -6,7 +6,14 @@ interface Text {
   author: string;
   image: string;
 }
+
+const defaultImage = "https://i.ibb.co/MDpY7RL/No-image-found.jpg";
+
 export const createText = (text: Text) => {
+  if (!text.image) {
+    text.image = defaultImage;
+  }
+
   const newText = textRepository.createText(text);
 
   return newText;
