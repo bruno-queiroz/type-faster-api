7ed73e8734aae0a1a51df296d83c1646ed41495a
bruno <brunoqueiroz612@gmail.com>
2023-10-12 16:16:28 -0300

    Image field accepts null or string

--- src/validators/createTextValidator.ts
+++ src/validators/createTextValidator.ts
@@ -4,5 +4,10 @@ export const createTextValidator = [
   body("text").notEmpty().isString(),
   body("author").notEmpty().isString(),
   body("title").notEmpty().isString(),
-  body("image").notEmpty().isString(),
+  body("image").custom((value) => {
+    if (value === null || (typeof value === "string" && value.trim() !== "")) {
+      return true;
+    }
+    throw new Error("image field should be either null or a non-empty string");
+  }),
 ];
