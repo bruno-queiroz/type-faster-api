8c19da62925b1822ef66664fc34312c84250ce75
bruno <brunoqueiroz612@gmail.com>
2023-10-16 08:30:45 -0300

    Change image object key to picture

--- src/services/createUser.ts
+++ src/services/createUser.ts
@@ -1,11 +1,14 @@
-import { User, userRepository } from "../repositories/userRepository";
+import { UserBody } from "../controllers/createUser";
+import { userRepository } from "../repositories/userRepository";
 import bcrypt from "bcrypt";
 
-export const createUser = async (user: User) => {
-  const passwordHashed = await bcrypt.hash(
-    user.password || Math.random().toString(),
-    10
-  );
+export const createUser = async (user: UserBody) => {
+  const passwordHashed = await bcrypt.hash("123", 10);
+
+  if (user.image) {
+    user.picture = user.image;
+    delete user.image;
+  }
 
   const newUser = await userRepository.createUser({
     ...user,
