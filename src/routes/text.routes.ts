009d80166506cec3f66db09c7338953e41c14336
bruno <brunoqueiroz612@gmail.com>
2023-10-19 08:18:29 -0300

    Create get text GET route

--- src/routes/text.routes.ts
+++ src/routes/text.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { createTextValidator } from "../validators/createTextValidator";
 import { createTextController } from "../controllers/createText";
+import { getTextController } from "../controllers/getText";
 
 const router = Router();
 
 router.post("/", createTextValidator, createTextController);
+router.get("/", getTextController);
 
 export default router;
