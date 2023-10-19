80a6d57177a19bf859780a76b41008f42d6fdecd
bruno <brunoqueiroz612@gmail.com>
2023-10-19 08:18:50 -0300

    Create getText service

---
+++ src/services/getText.ts
@@ -0,0 +1,8 @@
+import { textRepository } from "../repositories/textRepository";
+
+export const getText = async () => {
+  const text = await textRepository.getText();
+  const formattedText = { ...text[0], text: text[0].text.split("") };
+
+  return formattedText;
+};
