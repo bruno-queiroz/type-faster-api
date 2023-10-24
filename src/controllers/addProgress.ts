476867b315bd1bc910db9fa2445c944c69b556fe
bruno <brunoqueiroz612@gmail.com>
2023-10-24 08:25:49 -0300

    Create addProgress controller

---
+++ src/controllers/addProgress.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { addProgress } from "../services/addProgress";
+import { validationResult } from "express-validator";
+
+export interface TypingBody {
+  email: string;
+  textId: string;
+  cpm: string;
+}
+
+export const addProgressController = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .json({ errors: errors.array(), isOk: false, code: 400 });
+  }
+
+  try {
+    const addedTyping = await addProgress(req.body);
+
+    res
+      .status(201)
+      .json({ data: addedTyping, message: "Added to progress", isOk: true });
+  } catch (err) {
+    console.error("Error adding to progress", err);
+
+    res.status(500).json({
+      message: "Something went wrong adding progress",
+      isOk: false,
+      code: 500,
+    });
+  }
+};
