306a96ea180f93c0b9105418d53e5e7b239954c6
bruno <brunoqueiroz612@gmail.com>
2023-10-30 16:35:12 -0300

    Create getProgress route

--- src/routes/progress.routes.ts
+++ src/routes/progress.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { addProgressController } from "../controllers/addProgress";
 import { addProgressValidator } from "../validators/addProgressValidator";
+import { getProgressController } from "../controllers/getProgress";
 
 const router = Router();
 
 router.post("/", addProgressValidator, addProgressController);
+router.get("/:email", getProgressController);
 
 export default router;
