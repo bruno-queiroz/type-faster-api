69ceed1605e171b9b333f01dc6736a82040a9915
bruno <brunoqueiroz612@gmail.com>
2023-11-10 08:53:05 -0300

    Check validation errors

--- src/controllers/getText.ts
+++ src/controllers/getText.ts
@@ -1,7 +1,20 @@
 import { Request, Response } from "express";
 import { getText } from "../services/getText";
+import { validationResult } from "express-validator";
+import { getRepeatedWords } from "../services/getRepeatedWords";
 
 export const getTextController = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+      message: "Invalid query parameter.",
+      isOk: false,
+      code: 400,
+    });
+  }
+
   try {
     const text = await getText();
 
