291f6e86dffe986cc9d2f7fd3cb68fde7b0641d9
bruno <brunoqueiroz612@gmail.com>
2023-11-10 08:42:23 -0300

    Add  mode property to text

--- src/services/getText.ts
+++ src/services/getText.ts
@@ -2,7 +2,11 @@ import { textRepository } from "../repositories/textRepository";
 
 export const getText = async () => {
   const text = await textRepository.getText();
-  const formattedText = { ...text[0], text: text[0].text.split("") };
+  const formattedText = {
+    mode: "traditional",
+    ...text[0],
+    text: text[0].text.split(""),
+  };
 
   return formattedText;
 };
