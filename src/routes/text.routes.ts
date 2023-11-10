b7d3c4119c06482fe67b5d8b0d44ea7b9f486505
bruno <brunoqueiroz612@gmail.com>
2023-11-10 08:45:29 -0300

    Add getTextValidator

--- src/routes/text.routes.ts
+++ src/routes/text.routes.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { createTextValidator } from "../validators/createTextValidator";
 import { createTextController } from "../controllers/createText";
 import { getTextController } from "../controllers/getText";
+import { getTextValidator } from "../validators/getTextValidator";
 
 const router = Router();
 
 router.post("/", createTextValidator, createTextController);
-router.get("/", getTextController);
+router.get("/", getTextValidator, getTextController);
 
 export default router;
