16b480539638fa80f8a4acb9c05cc7e0d494b201
bruno <brunoqueiroz612@gmail.com>
2023-11-13 08:25:31 -0300

    Add /word route

--- src/routes/text.routes.ts
+++ src/routes/text.routes.ts
@@ -3,10 +3,13 @@ import { createTextValidator } from "../validators/createTextValidator";
 import { createTextController } from "../controllers/createText";
 import { getTextController } from "../controllers/getText";
 import { getTextValidator } from "../validators/getTextValidator";
+import { createWordValidator } from "../validators/createWordValidator";
+import { createWordController } from "../controllers/createWord";
 
 const router = Router();
 
 router.post("/", createTextValidator, createTextController);
+router.post("/word", createWordValidator, createWordController);
 router.get("/", getTextValidator, getTextController);
 
 export default router;
