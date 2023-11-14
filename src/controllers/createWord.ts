4bf84f4a7c3e7de3aa7b3d6ac2896ae452892a83
bruno <brunoqueiroz612@gmail.com>
2023-11-14 16:32:26 -0300

    Add word must be unique custom error

--- src/controllers/createWord.ts
+++ src/controllers/createWord.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { createWord } from "../services/createWord";
 import { validationResult } from "express-validator";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 export const createWordController = async (req: Request, res: Response) => {
   const errors = validationResult(req);
@@ -19,6 +20,17 @@ export const createWordController = async (req: Request, res: Response) => {
       .json({ data: addedWord, message: "Word added", isOk: true });
   } catch (err) {
     console.error("Error Adding word", err);
+
+    if (err instanceof PrismaClientKnownRequestError) {
+      if (err.code === "P2002") {
+        res.status(400).json({
+          isOk: false,
+          message: "Word must be unique",
+          code: 400,
+        });
+      }
+      return;
+    }
 
     res.status(500).json({
       message: "Something went wrong Adding word",
