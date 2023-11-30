20d9b7549fcb94ff427d328cbb0ec53d76e31626
bruno <brunoqueiroz612@gmail.com>
2023-11-30 07:27:28 -0300

    Return empty progress if user hasn't typed enough

--- src/services/getProgress.ts
+++ src/services/getProgress.ts
@@ -5,6 +5,9 @@ export const getProgress = async (email: string) => {
   const userId = await progressRepository.getUserId(email);
   const progress = await progressRepository.getProgress(userId.id);
 
+  if (progress.length < 4) {
+    return { overallAverageCpm: 0, overallAverageTypos: 0, progress: [] };
+  }
   const formattedProgress = formatProgressData(progress);
 
   return formattedProgress;
