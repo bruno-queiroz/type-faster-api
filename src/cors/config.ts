312ddd55bea3bb61a0d973a00d5b4aaffcda9314
bruno <brunoqueiroz612@gmail.com>
2023-12-05 07:18:28 -0300

    Allow undefined origin

--- src/cors/config.ts
+++ src/cors/config.ts
@@ -5,7 +5,7 @@ const allowedUrls = ["https://type-faster.vercel.app"];
 export const corsOptions: CorsOptions = {
   origin: function (origin, callback) {
     console.log("origin request:", origin);
-    if (allowedUrls.indexOf(origin || "") !== -1) {
+    if (allowedUrls.indexOf(origin || "") !== -1 || origin === undefined) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
